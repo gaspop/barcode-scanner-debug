196c2987b2a6a98e18fd2ea303d991ea84026e77
Christian Blomqvist <chrille.blomqvist@gmail.com>
2022-09-29 10:58:06 +0200

    Setup capacitor and barcode-scanner.

---
+++ android/app/src/main/java/com/barcodescannerdebug/MainActivity.java
@@ -0,0 +1,5 @@
+package com.barcodescannerdebug;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useEffect } from 'react'
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const testScan = async () => {
+      await BarcodeScanner.checkPermission({ force: true });
+
+      // make background of WebView transparent
+      // note: if you are using ionic this might not be enough, check below
+      BarcodeScanner.hideBackground();
+
+      await BarcodeScanner.startScan(); // start scanning and wait for a result
+    }
+
+    setTimeout(testScan, 3000);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
